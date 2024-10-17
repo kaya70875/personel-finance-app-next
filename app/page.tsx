96fa5b7a9c7cd945b2308b440c10a3561ecfbd1f
kaya70875 <shortsforturkey@gmail.com>
2024-10-17 22:46:28 +0300

    style home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import OverviewCard from "@components/OverviewCard";
+import './transactions/_global.scss';
+
 export default function Home() {
   return (
-    <div>
-      home
+    <div className="home">
+      <section className="page-header-section">
+        <h1>Overview</h1>
+      </section>
+
+      <section className="overview-content-section">
+        <div className="overview-cards">
+          <OverviewCard isActive={true} cardHeader="Current Balance" cardPrice={1000} />
+          <OverviewCard cardHeader="Income" cardPrice={232} />
+          <OverviewCard cardHeader="Expenses" cardPrice={2300} />
+        </div>
+      </section>
     </div>
   );
 }
