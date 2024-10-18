f2cdd3c6bec4dc227e39dd28b17c2f72fe48ed63
kaya70875 <shortsforturkey@gmail.com>
2024-10-18 13:27:52 +0300

    create cardVisuals component

---
+++ components/cards/CardVisuals.tsx
@@ -0,0 +1,20 @@
+import './styles/_CardVisuals.scss';
+
+interface CardVisualsProps {
+    cardVisualColor: string;
+    cardHeader: string;
+    cardPrice: number;
+}
+
+const CardVisuals = ({ cardVisualColor, cardHeader, cardPrice }: CardVisualsProps) => {
+  return (
+    <div className='card-visual-container' style={{'--card-visual-color': cardVisualColor} as React.CSSProperties}>
+        <div className="card-visual-info">
+            <p>{cardHeader}</p>
+            <h4>{cardPrice}</h4>
+        </div>
+    </div>
+  )
+}
+
+export default CardVisuals
