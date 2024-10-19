18a212949b3df2e2d09c523f4ebb6285708b0eaa
kaya70875 <shortsforturkey@gmail.com>
2024-10-19 20:08:04 +0300

    creating types

---
+++ types/finance.ts
@@ -0,0 +1,28 @@
+export interface Balance {
+    current : number;
+    income : number;
+    expenses : number;
+}
+
+export interface Budgets {
+    category : string;
+    maximum : number;
+    themen : string;
+    percentage : number;
+}
+
+export interface Transactions {
+    avatar : string;
+    name : string;
+    category : string;
+    date : Date;
+    amount : number;
+    recurring : boolean;
+}
+
+export interface Pots {
+    name : string;
+    target : number;
+    total : number;
+    theme : string;
+}
