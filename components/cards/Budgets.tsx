706944296c9b9a8ac52342884b209aacde0c3cfe
kaya70875 <shortsforturkey@gmail.com>
2024-10-20 14:22:47 +0300

    create pieChart

--- components/cards/Budgets.tsx
+++ components/cards/Budgets.tsx
@@ -1,24 +1,45 @@
 import DetailsLink from '@components/links/DetailsLink';
 import React from 'react'
 import './styles/_Budgets.scss';
-import BudgetsGraph from '@components/graphs/PieChart';
+import PieChart from '@components/graphs/PieChart';
+import CardVisuals from './CardVisuals';
+import useFetch from '@hooks/useFetch';
 
 interface BudgetsProps {
-    headerSection : boolean;
-    gridType : string;
+    headerSection: boolean;
+    gridType: string;
 }
 
-export default function Budgets({headerSection , gridType} : BudgetsProps) {
-  return (
-    <div className="budgets-wrapper">
-        {headerSection && (
-            <header className="budgets-inline-header">
-                <h2>Budgets</h2>
-                <DetailsLink href='/budgets'/>
-            </header>
-        )}
+export default function Budgets({ headerSection, gridType }: BudgetsProps) {
 
-        <BudgetsGraph />
-    </div>
-  )
+    const { data, error, loading } = useFetch();
+    if (loading) return <div>loading</div>
+    if (error) return <div>{error}</div>
+
+
+    return (
+        <div className="budgets-wrapper">
+            {headerSection && (
+                <header className="budgets-inline-header">
+                    <h3>Budgets</h3>
+                    <DetailsLink href='/budgets' />
+                </header>
+            )}
+            <div className="chart-main">
+                <PieChart />
+                <div className="budgets-visuals">
+                    {data?.budgetsData.map(budget => (
+                        <CardVisuals
+                            cardHeader={budget.category}
+                            cardPrice={budget.maximum}
+                            cardVisualColor={budget.theme}
+                        />
+                    ))}
+                </div>
+
+
+            </div>
+
+        </div>
+    )
 }
