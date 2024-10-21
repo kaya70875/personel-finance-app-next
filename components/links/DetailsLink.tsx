485997c5491c4c779e31e9f2be76b3e5663ad371
kaya70875 <shortsforturkey@gmail.com>
2024-10-21 20:23:29 +0300

    create detailsLink component

--- components/links/DetailsLink.tsx
+++ components/links/DetailsLink.tsx
@@ -3,11 +3,16 @@ import React from 'react'
 import rightArrow from '@public/assets/images/icon-caret-right.svg';
 import Image from 'next/image';
 
-export default function DetailsLink({href} : {href : string}) {
+interface DetailsLinkProps {
+    href: string;
+    header? : string;
+}
+
+export default function DetailsLink({href , header = 'See Details'} : DetailsLinkProps) {
     return (
         <>
 
-            <Link href={href}>See Details <span style={{ marginLeft: '.75em' }}>
+            <Link href={href}>{header}<span style={{ marginLeft: '.75em' }}>
                 <Image src={rightArrow}
                     width={10}
                     height={10}
