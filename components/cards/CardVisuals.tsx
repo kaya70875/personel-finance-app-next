4880838e2bcfbef6257690e10f3328a8734c0632
kaya70875 <shortsforturkey@gmail.com>
2024-10-23 18:58:56 +0300

    add cardSpend to cardVisuals

--- components/cards/CardVisuals.tsx
+++ components/cards/CardVisuals.tsx
@@ -1,20 +1,23 @@
 import './styles/_CardVisuals.scss';
 
 interface CardVisualsProps {
-    cardVisualColor: string;
-    cardHeader: string;
-    cardPrice: number;
+  cardVisualColor: string;
+  cardHeader: string;
+  cardPrice: number;
+  cardSpend?: number;
 }
 
-const CardVisuals = ({ cardVisualColor, cardHeader, cardPrice }: CardVisualsProps) => {
+const CardVisuals = ({ cardVisualColor, cardHeader, cardPrice, cardSpend }: CardVisualsProps) => {
   return (
-    <div className='card-visual-container' style={{'--card-visual-color': cardVisualColor} as React.CSSProperties}>
-        <div className="card-visual-info">
-            <p>{cardHeader}</p>
-            <h4>${cardPrice}</h4>
+    <div className='card-visual-container' style={{ '--card-visual-color': cardVisualColor } as React.CSSProperties}>
+      <div className="card-visual-info">
+        <p>{cardHeader}</p>
+        <div className="card-extra-info">
+          <h4>${cardPrice}</h4>
         </div>
+      </div>
     </div>
   )
 }
 
-export default CardVisuals
+export default CardVisuals;
