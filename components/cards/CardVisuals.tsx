a76480ec638dd60293e2ef81d833613e82176140
kaya70875 <shortsforturkey@gmail.com>
2024-10-24 19:02:18 +0300

    add variaton to the cardVisuals component

--- components/cards/CardVisuals.tsx
+++ components/cards/CardVisuals.tsx
@@ -5,15 +5,27 @@ interface CardVisualsProps {
   cardHeader: string;
   cardPrice: number;
   cardSpend?: number;
+  isFullPage?: boolean;
 }
 
-const CardVisuals = ({ cardVisualColor, cardHeader, cardPrice, cardSpend }: CardVisualsProps) => {
+const CardVisuals = ({ cardVisualColor, cardHeader, cardPrice, cardSpend, isFullPage }: CardVisualsProps) => {
   return (
-    <div className='card-visual-container' style={{ '--card-visual-color': cardVisualColor } as React.CSSProperties}>
+    <div className={`card-visual-container ${isFullPage ? 'card-visual-container--main' : ''}`} style={{ '--card-visual-color': cardVisualColor } as React.CSSProperties}>
       <div className="card-visual-info">
         <p>{cardHeader}</p>
         <div className="card-extra-info">
-          <h4>${cardPrice}</h4>
+          {isFullPage ? (
+            <>
+              <h3>${cardSpend}
+              </h3>
+              <p>
+                of ${cardPrice}
+              </p>
+            </>
+
+          ) : (
+            <h3>${cardPrice}</h3>
+          )}
         </div>
       </div>
     </div>
