d05482c1828e4fe45db8521ae1e4eb0427103c45
kaya70875 <shortsforturkey@gmail.com>
2024-10-24 19:04:58 +0300

    update style

--- app/budgets/page.tsx
+++ app/budgets/page.tsx
@@ -2,21 +2,47 @@
 
 import Budgets from '@components/cards/Budgets';
 import './_global.scss';
+import BudgetsInfoCard from '@components/cards/BudgetsInfoCard';
+import useFetch from '@hooks/useFetch';
 
 export default function page() {
+  const { data, loading, error } = useFetch();
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>Error</div>
+
+  const transactionsData = data?.transactionsData ?? [];
+  const budgetsData = data?.budgetsData ?? [];
+
   return (
     <div className="home">
-        <header className="page-header">
-            <h1>Budgets</h1>
-        </header>
-        <div className="budgets-main">
-            <Budgets headerSection={false}
-             chartWidth={'50%'}
-             />
-            <div className="budget-cards">
+      <header className="page-header">
+        <h1>Budgets</h1>
+      </header>
+      <div className="budgets-main">
+        <Budgets headerSection={false}
+          isFullPage={true}
+        />
+        <div className="budgets-cards">
+          {budgetsData.map(budget => {
+            const filteredTransactions = transactionsData.filter(
+              transaction => transaction.category === budget.category
+            );
 
-            </div>
+            return (
+              <BudgetsInfoCard
+                key={budget.category}
+                category={budget.category}
+                cardTheme={budget.theme}
+                maximum={budget.maximum}
+                spend={budget.spend}
+                posts={filteredTransactions}
+              />
+            );
+          })}
+
         </div>
+      </div>
     </div>
   )
 }
