6400ca2baa5ca24a9508c4c4b759f5435222f889
kaya70875 <shortsforturkey@gmail.com>
2024-10-25 19:56:33 +0300

    update overviewCard component

--- components/OverviewCard.tsx
+++ components/OverviewCard.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import '@components/styles/_OverviewCard.scss'
+import Image from 'next/image';
+
+import billImage from '@public/assets/images/icon-recurring-bills.svg';
 
 interface OverviewCardProps {
     cardHeader : string;
     cardPrice : number;
     isActive? : boolean;
+    hasImage? : boolean;
 }
 
-const OverviewCard = ({cardHeader , cardPrice , isActive} : OverviewCardProps) => {
+const OverviewCard = ({cardHeader , cardPrice , isActive , hasImage} : OverviewCardProps) => {
   return (
     <div className={`overview-card-wrapper ${isActive ? 'active' : ''}`}>
+        {hasImage && (
+          <Image 
+          src={billImage}
+          alt='bill'
+          width={48}
+          height={48}/>
+        )}
         <header className="overview-card-header-section">
             {cardHeader}
         </header>
