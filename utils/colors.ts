c6d96b23e57daa140240ea23bc304f1337d52fa9
kaya70875 <shortsforturkey@gmail.com>
2024-10-26 16:03:44 +0300

    create colors for using

---
+++ utils/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+    Gray: '#696868',
+    Green: '#277C78',
+    Yellow: '#F2CDAC',
+    Cyan: '#82C9D7',
+    Navy: '#626070',
+    Red: '#C94736',
+    Purple: '#AF81BA',
+    Turquoise: '#597C7C',
+    Brown: '#93674F',
+    Magenta: '#934F6F',
+    Blue: '#3F82B2',
+    NavyGrey: '#97A0AC',
+    ArmyGreen: '#7F9161',
+    Gold: '#CAB361',
+    Orange: '#BE6C49'
+}
