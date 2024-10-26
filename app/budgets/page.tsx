914ef705e32f9be12a990c1c308a6fa1bf77e1e7
kaya70875 <shortsforturkey@gmail.com>
2024-10-26 16:05:15 +0300

    create , style and initialize modal component

--- app/budgets/page.tsx
+++ app/budgets/page.tsx
@@ -5,21 +5,62 @@ import './_global.scss';
 import BudgetsInfoCard from '@components/cards/BudgetsInfoCard';
 import useFetch from '@hooks/useFetch';
 import AddButton from '@components/buttons/AddButton';
+import { useState } from 'react';
+import Modal from '@components/cards/Modal';
+import Dropdown from '@components/dropdowns/Dropdown';
+import { getUniqueCategories } from '@utils/helpers';
+import { colors } from '@utils/colors';
 
 export default function page() {
   const { data, loading, error } = useFetch();
 
-  if (loading) return <div>Loading...</div>
-  if (error) return <div>Error</div>
-
   const transactionsData = data?.transactionsData ?? [];
   const budgetsData = data?.budgetsData ?? [];
+
+  const [isPopped, setIsPopped] = useState(false);
+
+  const handleClick = () => {
+    setIsPopped(prev => !prev);
+  }
+
+  const uniqueCategories = getUniqueCategories(transactionsData);
 
   return (
     <div className="home">
       <header className="page-header">
         <h1>Budgets</h1>
-        <AddButton buttonText='Budget'/>
+        <button className="add-button" onClick={handleClick}>+ Add New Budget</button>
+        {isPopped && (
+          <Modal onClose={setIsPopped} modalHeaderText={'Budget'} modalDesc={'Choose a category to set a spending budget. These categories can help you monitor spending.'}>
+            <div className="inputs-wrapper">
+              <div className="modal-input">
+                <label>Budget Category</label>
+                <Dropdown buttonName='Entertainment'>
+                  {uniqueCategories.map(category => (
+                    <p key={category}>{category}</p>
+                  ))}
+                </Dropdown>
+
+                <label htmlFor="spend">Maximum Spend</label>
+                <input type="text" placeholder='$ e.g.2000' className='modal-input-item' />
+
+                <label>Theme</label>
+                <Dropdown buttonName={
+                  'Choose a color'
+                }>
+                  {Object.entries(colors).map(([colorName, colorValue]) => (
+                    <div key={colorName} className='color-option'>
+                      <div className='ellipse' style={{ backgroundColor: colorValue }}></div>
+                      <p>{colorName}</p>
+                    </div>
+                  ))}
+                </Dropdown>
+              </div>
+            </div>
+            <button className="add-button">Add Budget</button>
+          </Modal>
+        )}
+
       </header>
       <div className="budgets-main">
         <Budgets headerSection={false}
---
+++ components/cards/Modal.tsx
@@ -0,0 +1,28 @@
+import SvgIcon from '@components/reusables/SvgIcon'
+import './styles/_Modal.scss'
+
+interface ModalProps {
+  onClose: React.Dispatch<React.SetStateAction<boolean>>
+  modalHeaderText : string;
+  modalDesc : string;
+  children : React.ReactNode;
+}
+export default function Modal({onClose , modalHeaderText , modalDesc , children} : ModalProps) {
+  return (
+    <>
+      <div className="modal-backdrop"></div>
+      <div className="modal-overlay">
+        <header className="modal-header">
+          <h2>Add New {modalHeaderText}</h2>
+          <div className="modal-close-button" onClick={() => onClose(false)}>
+            <SvgIcon path={"close-modal"}/>
+          </div>
+        </header>
+        <div className="modal-desc">
+          <p>{modalDesc}</p>
+        </div>
+        {children}
+      </div>
+    </>
+  )
+}
