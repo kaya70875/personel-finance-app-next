ec44c576c942a0c89dc2d5685b2cedd50312519a
kaya70875 <shortsforturkey@gmail.com>
2024-10-27 16:56:44 +0300

    add route for adding new budgets

---
+++ app/api/addBudget/route.ts
@@ -0,0 +1,26 @@
+import budgets from "@models/budgets";
+import { connectToDB } from "@utils/database";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    try {        
+        await connectToDB();
+
+        const body = await req.json();
+        const { category, maximum, theme } = body;
+
+        const newBudget = await budgets.create({category , maximum, theme});
+        console.log(newBudget);
+
+        return NextResponse.json({
+            message: "Budget added successfully",
+            data: newBudget,
+        });
+
+    } catch(e) {
+        console.log(e);
+        return NextResponse.json({
+            message: "Error adding budget",
+        });
+    }
+}
