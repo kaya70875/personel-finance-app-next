8b823d7588be6c1068e003ddf1d198410705acae
kaya70875 <shortsforturkey@gmail.com>
2024-10-29 19:51:45 +0300

    update useDelete hook

--- hooks/useDelete.tsx
+++ hooks/useDelete.tsx
@@ -1,31 +1,38 @@
-import { useSWRConfig } from "swr"
+import { useSWRConfig } from "swr";
 
 interface DeleteHandler {
-    deleteItem : (id : string , url : string) => Promise<void>;
+    deleteItem: (id: string, url: string) => Promise<void>;
 }
 
-const useDelete = () => {
-    const {mutate} = useSWRConfig();
+const useDelete = (): DeleteHandler => {
+    const { mutate } = useSWRConfig();
 
+    /**
+     * Deletes a pot or budget card based on given ID and endpoint.
+     *
+     * @param {string} id - ID for the relevant card object.
+     * @param {string} url - API endpoint (relative path) where the card data should be sent.
+     */
     const deleteItem: DeleteHandler['deleteItem'] = async (id, url) => {
         try {
             const response = await fetch(`/api/delete${url}`, {
-              method: 'DELETE',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify({ id }),
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ id }),
             });
-            
+
             if (response.ok) {
-              console.log(`${url} deleted successfully`);
-              await mutate('/api/getData');
+                console.log(`${url} deleted successfully`);
+                await mutate('/api/getData');
             } else {
-              console.error(`Failed to delete ${url}`);
+                console.error(`Failed to delete ${url}`);
             }
-          } catch (error) {
+        } catch (error) {
             console.error('Error:', error);
-          }
-    }
+        }
+    };
+
     return { deleteItem };
-}
+};
 
 export default useDelete;
