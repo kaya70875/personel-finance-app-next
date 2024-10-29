205baa85aa80c6e48d5dfa0c9d83f5208cfce6e2
kaya70875 <shortsforturkey@gmail.com>
2024-10-29 19:54:27 +0300

    create delete modal component

--- components/dropdowns/EditDropdown.tsx
+++ components/dropdowns/EditDropdown.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import editButton from '@public/assets/images/icon-ellipsis.svg';
 import { useState } from 'react';
 import useDelete from '@hooks/useDelete';
+import Modal from '@components/cards/Modal';
 
 interface EditDropdownProps {
     header: string;
@@ -16,6 +17,7 @@ interface EditDropdownProps {
 export default function EditDropdown({ header , id , type}: EditDropdownProps) {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [isPopped , setIsPopped] = useState(false);
     const { deleteItem } = useDelete();
 
     const handleOpen = () => {
@@ -42,10 +44,17 @@ export default function EditDropdown({ header , id , type}: EditDropdownProps) {
                 <ul className="edit-dropdown-items">
                     <li className="edit-dropdown-item">
                         <p>Edit {header}</p>
-                        <p className='red' onClick={handleDelete}>Delete {header}</p>
+                        <p className='red' onClick={() => setIsPopped(true)}>Delete {header}</p>
                     </li>
                 </ul>
             </div>
+            {isPopped && (
+                <Modal modalHeaderText={`Delete '${header}' ?`} modalDesc={`Are you sure you want to delete this ${header} ? This action cannot be reversed , and all the
+                data inside it will be removed forever.`} onClose={setIsPopped}>
+                    <button onClick={handleDelete} className="card-delete">Yes , Confirm Deletion</button>
+                    <button onClick={() => setIsPopped(false)} className="card-back">No , Go Back</button>
+                </Modal>
+            )}
         </div>
     )
 }
