eb4022ea488cffcb19ce532024db6d941fe32414
kaya70875 <shortsforturkey@gmail.com>
2024-10-31 15:42:06 +0300

    create cardMappings for handling multiple card actions

---
+++ utils/cardMappings.ts
@@ -0,0 +1,14 @@
+// cardMappings.ts
+export const cardMappings = {
+    budget: {
+      name: 'category',
+      price: 'maximum',
+      theme: 'theme',
+    },
+    pot: {
+      name: 'name',
+      price: 'target',
+      theme: 'theme',
+    },
+  } as const;
+  
