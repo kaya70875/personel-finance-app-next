d871d5709eb9c206b6ab7fb4c9ab637338f4054c
kaya70875 <shortsforturkey@gmail.com>
2024-10-31 15:43:19 +0300

    use a new component to budgets page

--- app/budgets/page.tsx
+++ app/budgets/page.tsx
@@ -5,40 +5,20 @@ import './_global.scss';
 import BudgetsInfoCard from '@components/cards/BudgetsInfoCard';
 import { useState } from 'react';
 import Modal from '@components/cards/Modal';
-import Dropdown from '@components/dropdowns/Dropdown';
-import { getUniqueCategories } from '@utils/helpers';
-import { colors } from '@utils/colors';
 import useFetch from '@hooks/useFetch';
-import useAdd from '@hooks/useAdd';
+import ModalClassic from '@components/cards/modal/ModalClassic';
 
 export default function page() {
   const { data, loading, error } = useFetch();
-  const { handleAddCard } = useAdd();
-
-  const [budgetData, setBudgetData] = useState({
-    category: '',
-    maximum: 0,
-    theme: '',
-  });
 
   const transactionsData = data?.transactionsData ?? [];
   const budgetsData = data?.budgetsData ?? [];
 
-  const [isPopped, setIsPopped] = useState(false);
-  const [activeDropdownItem, setActiveDropdownItem] = useState({
-    budgetCategory: 'Choose a category',
-    budgetTheme: 'Choose a color',
-  });
+  const [isPopped, setIsPopped] = useState<boolean>(false);
 
   const handleClick = () => {
     setIsPopped(prev => !prev);
   }
-
-  const handleAddBudget = () => {
-    handleAddCard(budgetData, 'addBudget', setIsPopped);
-  };
-
-  const uniqueCategories = getUniqueCategories(transactionsData);
 
   return (
     <div className="home">
@@ -47,55 +27,7 @@ export default function page() {
         <button className="add-button" onClick={handleClick}>+ Add New Budget</button>
         {isPopped && (
           <Modal onClose={setIsPopped} modalHeaderText={'Add New Budget'} modalDesc={'Choose a category to set a spending budget. These categories can help you monitor spending.'}>
-            <div className="inputs-wrapper">
-              <div className="modal-input">
-                <label>Budget Category</label>
-                <Dropdown buttonName={activeDropdownItem.budgetCategory}>
-                  {uniqueCategories.map(category => (
-                    <li className='dropdown-item' key={category} onClick={() => {
-                      setBudgetData({
-                        ...budgetData,
-                        category: category,
-                      })
-                      setActiveDropdownItem({
-                        ...activeDropdownItem,
-                        budgetCategory: category,
-                      });
-                    }}>
-                      <p>{category}</p>
-                    </li>
-                  ))}
-                </Dropdown>
-
-                <label htmlFor="spend">Maximum Spend</label>
-                <input type="numeric" placeholder='$ e.g.2000' className='modal-input-item' onChange={(e) => setBudgetData({
-                  ...budgetData,
-                  maximum: parseInt(e.target.value),
-                })} />
-
-                <label>Theme</label>
-                <Dropdown buttonName={
-                  activeDropdownItem.budgetTheme
-                }>
-                  {Object.entries(colors).map(([colorName, colorValue]) => (
-                    <li key={colorName} className='color-option' onClick={() => {
-                      setBudgetData({
-                        ...budgetData,
-                        theme: colorValue,
-                      })
-                      setActiveDropdownItem({
-                        ...activeDropdownItem,
-                        budgetTheme: colorName,
-                      });
-                    }}>
-                      <div className='ellipse' style={{ backgroundColor: colorValue }}></div>
-                      <p>{colorName}</p>
-                    </li>
-                  ))}
-                </Dropdown>
-              </div>
-            </div>
-            <button className="add-button" onClick={handleAddBudget}>Add Budget</button>
+            <ModalClassic cardType='budget' setIsPopped={setIsPopped} actionType='add'/>
           </Modal>
         )}
 
@@ -115,6 +47,7 @@ export default function page() {
                 key={budget._id}
                 budget={budget}
                 filteredTransactions={filteredTransactions}
+                cardType='budget'
               />
             );
           })}
