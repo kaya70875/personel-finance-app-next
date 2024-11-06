bb3ba35a25a699f9cd6ccd4cb802b0a81e3e0e23
kaya70875 <shortsforturkey@gmail.com>
2024-11-06 13:33:00 +0300

    handling form actions

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -5,8 +5,24 @@ import Image from 'next/image';
 
 import logo from '@public/assets/images/logo-large.svg';
 import SvgIcon from '@components/reusables/SvgIcon';
+import { useState } from 'react';
+import { useAuthActions } from '@hooks/useAuthActions';
 
 export default function page() {
+
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        password: '',
+    })
+
+    const { handleSignUp } = useAuthActions();
+
+    const handleClick = () => {
+        handleSignUp(form);
+        alert('account created.');
+    }
+
     return (
         <div className="auth-wrapper">
             <section className="auth-illustration">
@@ -29,24 +45,24 @@ export default function page() {
                     <form className='auth-form'>
                         <div className="form-input-wrapper">
                             <label htmlFor="name">Name</label>
-                            <input className='modal-input-item' type="text" id='name' />
+                            <input className='modal-input-item' type="text" id='name' onChange={(e) => setForm({ ...form, name: e.currentTarget.value })} />
                         </div>
 
                         <div className="form-input-wrapper">
                             <label htmlFor="email">Email</label>
-                            <input className='modal-input-item' type="text" id='email' />
+                            <input className='modal-input-item' type="text" id='email' onChange={(e) => setForm({ ...form, email: e.currentTarget.value })} />
                         </div>
 
                         <div className="form-input-wrapper">
                             <label htmlFor="pass">Create Password</label>
                             <div className="password-input-wrapper">
-                                <input className='modal-input-item' type="text" id='pass' />
+                                <input className='modal-input-item' type="text" id='pass' onChange={(e) => setForm({ ...form, password: e.currentTarget.value })} />
                                 <SvgIcon path='show-password' />
                             </div>
                             <p>Password must be at least 8 characters</p>
                         </div>
 
-                        <button className="add-button">Create Account</button>
+                        <button className="add-button" onClick={(handleClick)}>Create Account</button>
 
                         <div className="have-account">
                             <p>Already have an account ?</p>
