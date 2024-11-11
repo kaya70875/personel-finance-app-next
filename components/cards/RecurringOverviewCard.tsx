d1721752f9b9829b9e98647dc3c90b40aa1c2d63
kaya70875 <shortsforturkey@gmail.com>
2024-11-11 15:00:11 +0300

    add skeleton for overview recurrings

--- components/cards/RecurringOverviewCard.tsx
+++ components/cards/RecurringOverviewCard.tsx
@@ -1,33 +1,43 @@
-import DetailsLink from '@components/links/DetailsLink';
 import './styles/_RecurringOverviewCard.scss';
 import { useTransactionDetails } from '@context/RecurBillsContext';
 import { formatCurrency } from '@utils/helpers';
+import OverviewCardHeaderSection from '@components/reusables/OverviewCardHeaderSection';
+import OverviewSkeleton from '@components/skeletons/OverviewSkeleton';
 
 export default function RecurringOverviewCard() {
 
-    const {dueTotal , paidTotal , totalCount} = useTransactionDetails();
+    const { dueTotal, paidTotal, totalCount, loading, error } = useTransactionDetails();
 
-  return (
-    <div className="recurring-overview-card-wrapper">
-        <header className="overview-card-header-section">
-            <h3>Recurring Bills</h3>
-            <DetailsLink href='/recurring' header='See Details'/>
-        </header>
+    if (error) {
+        return <div>An error occured!</div>
+    }
+    return (
+        <div className="recurring-overview-card-wrapper">
+            <header className="overview-card-header-section">
+                <OverviewCardHeaderSection loading={loading} href='/recurrings' name='Recurring Bills' />
+            </header>
 
-        <div className="recurring-overview-cards">
-            <div className="recurring-overview-card">
-                <p>Paid Bills</p>
-                <h3>{formatCurrency(paidTotal)}</h3>
-            </div>
-            <div className="recurring-overview-card">
-                <p>Total Upcomings</p>
-                <h3>{formatCurrency(totalCount)}</h3>
-            </div>
-            <div className="recurring-overview-card">
-                <p>Due Soon</p>
-                <h3>{formatCurrency(dueTotal)}</h3>
+            <div className="recurring-overview-cards">
+                {loading ? (
+                    <OverviewSkeleton height='2rem' count={3} />
+                ) : (
+                    <>
+                        <div className="recurring-overview-card">
+                            <p>Paid Bills</p>
+                            <h3>{formatCurrency(paidTotal)}</h3>
+                        </div>
+                        <div className="recurring-overview-card">
+                            <p>Total Upcomings</p>
+                            <h3>{formatCurrency(totalCount)}</h3>
+                        </div>
+                        <div className="recurring-overview-card">
+                            <p>Due Soon</p>
+                            <h3>{formatCurrency(dueTotal)}</h3>
+                        </div>
+                    </>
+                )}
+
             </div>
         </div>
-    </div>
-  )
+    )
 }
