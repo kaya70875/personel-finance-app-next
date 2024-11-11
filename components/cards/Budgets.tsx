21edd0097a4f18a8138255be7e03021107ccadb3
kaya70875 <shortsforturkey@gmail.com>
2024-11-11 14:59:35 +0300

    add skeleton for overview-budgets

--- components/cards/Budgets.tsx
+++ components/cards/Budgets.tsx
@@ -1,9 +1,10 @@
-import DetailsLink from '@components/links/DetailsLink';
 import React from 'react'
 import './styles/_Budgets.scss';
 import PieChart from '@components/graphs/PieChart';
 import CardVisuals from './CardVisuals';
 import useFetch from '@hooks/useFetch';
+import OverviewCardHeaderSection from '@components/reusables/OverviewCardHeaderSection';
+import Skeleton from 'react-loading-skeleton';
 
 interface BudgetsProps {
     headerSection: boolean;
@@ -13,7 +14,6 @@ interface BudgetsProps {
 export default function Budgets({ headerSection, isFullPage = false }: BudgetsProps) {
 
     const { data, error, loading } = useFetch();
-    if (loading) return <div>loading</div>
     if (error) return <div>{error}</div>
 
 
@@ -21,29 +21,42 @@ export default function Budgets({ headerSection, isFullPage = false }: BudgetsPr
         <div className={`budgets-wrapper ${isFullPage ? 'budgets-wrapper--main' : ''}`}>
             {headerSection && (
                 <header className="budgets-inline-header">
-                    <h3>Budgets</h3>
-                    <DetailsLink href='/budgets' />
+                    <OverviewCardHeaderSection name='Budgets' loading={loading} href="/budgets" />
                 </header>
             )}
 
             <div className='chart-main'>
-                <PieChart />
+                {loading ? (
+                    <Skeleton width={'128px'} height={'128px'} circle />
+                ) : (
+                    <PieChart />
+                )}
                 <div className='budget-visuals'>
                     {isFullPage && (
                         <div className='spending-summary'>
                             <h3>Spending Summary</h3>
                         </div>
                     )}
-                    {data?.budgetsData.map((budget, index) => (
-                        <CardVisuals
-                            cardHeader={budget.category}
-                            cardPrice={budget.maximum}
-                            cardVisualColor={budget.theme}
-                            cardSpend={budget.spend}
-                            isFullPage={isFullPage}
-                            key={index}
-                        />
-                    ))}
+                    {loading ? (
+                        <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem', width : '125px' }}>
+                            <Skeleton width={'100%'} height={'20px'} />
+                            <Skeleton width={'100%'} height={'20px'} />
+                            <Skeleton width={'100%'} height={'20px'} />
+                            <Skeleton width={'100%'} height={'20px'} />
+                        </div>
+                    ) : (
+                        data?.budgetsData.map((budget, index) => (
+                            <CardVisuals
+                                cardHeader={budget.category}
+                                cardPrice={budget.maximum}
+                                cardVisualColor={budget.theme}
+                                cardSpend={budget.spend}
+                                isFullPage={isFullPage}
+                                key={index}
+                            />
+                        ))
+                    )}
+
                 </div>
 
 
