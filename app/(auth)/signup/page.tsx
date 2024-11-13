0f4e65c56e5168bc8e6c8ee8408b2a628a1feb43
kaya70875 <shortsforturkey@gmail.com>
2024-11-13 18:13:08 +0300

    create form validation

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -2,25 +2,116 @@
 
 import './_global.scss';
 import Image from 'next/image';
-
 import logo from '@public/assets/images/logo-large.svg';
 import SvgIcon from '@components/reusables/SvgIcon';
-import { useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { useAuthActions } from '@hooks/useAuthActions';
+import { useRouter } from 'next/navigation';
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordRegex = /^(?=.*\d)[A-Za-z\d]{8,}$/;
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    validEmail: false,
+    validatePassword: false,
+    error: '',
+    loading: false,
+};
+
+const enum REDUCER_ACTION_TYPE {
+    SET_NAME,
+    SET_EMAIL,
+    SET_PASSWORD,
+    SET_VALID_EMAIL,
+    SET_VALID_PASSWORD,
+    SET_ERROR,
+    SET_LOADING,
+}
+
+type ReducerAction =
+    | {
+        type: REDUCER_ACTION_TYPE.SET_NAME,
+        payload: string,
+    }
+    | {
+        type: REDUCER_ACTION_TYPE.SET_EMAIL,
+        payload: string,
+    }
+    | {
+        type: REDUCER_ACTION_TYPE.SET_PASSWORD,
+        payload: string,
+    }
+    | {
+        type: REDUCER_ACTION_TYPE.SET_VALID_EMAIL,
+        payload: boolean,
+    }
+    | {
+        type: REDUCER_ACTION_TYPE.SET_VALID_PASSWORD,
+        payload: boolean,
+    }
+    | {
+        type: REDUCER_ACTION_TYPE.SET_ERROR,
+        payload: string,
+    }
+    | {
+        type: REDUCER_ACTION_TYPE.SET_LOADING,
+        payload: boolean,
+    }
+
+const reducer = (state: typeof initialState, action: ReducerAction): typeof initialState => {
+    switch (action.type) {
+        case REDUCER_ACTION_TYPE.SET_NAME:
+            return { ...state, name: action.payload };
+        case REDUCER_ACTION_TYPE.SET_EMAIL:
+            return { ...state, email: action.payload, error: '' };
+        case REDUCER_ACTION_TYPE.SET_PASSWORD:
+            return { ...state, password: action.payload, error: '' };
+        case REDUCER_ACTION_TYPE.SET_VALID_EMAIL:
+            return { ...state, validEmail: action.payload };
+        case REDUCER_ACTION_TYPE.SET_VALID_PASSWORD:
+            return { ...state, validatePassword: action.payload };
+        case REDUCER_ACTION_TYPE.SET_ERROR:
+            return { ...state, error: action.payload };
+        case REDUCER_ACTION_TYPE.SET_LOADING:
+            return { ...state, loading: action.payload };
+        default:
+            return state;
+    }
+}
 
 export default function page() {
-
-    const [form, setForm] = useState({
-        name: '',
-        email: '',
-        password: '',
-    })
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const router = useRouter();
 
     const { handleSignUp } = useAuthActions();
 
-    const handleClick = () => {
-        handleSignUp(form);
-        alert('account created.');
+    useEffect(() => {
+        dispatch({ type: REDUCER_ACTION_TYPE.SET_VALID_EMAIL, payload: emailRegex.test(state.email) });
+    }, [state.email]);
+
+    useEffect(() => {
+        dispatch({ type: REDUCER_ACTION_TYPE.SET_VALID_PASSWORD, payload: passwordRegex.test(state.password) });
+    }, [state.password]);
+
+    const handleClick = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (!state.name || !state.email || !state.password) {
+            return dispatch({ type: REDUCER_ACTION_TYPE.SET_ERROR, payload: 'Please fill all forms!' });
+        }
+
+        if (state.validEmail && state.validatePassword) {
+            dispatch({ type: REDUCER_ACTION_TYPE.SET_LOADING, payload: true }); // Start Loading State.
+            await handleSignUp({ name: state.name, email: state.email, password: state.password });
+            dispatch({ type: REDUCER_ACTION_TYPE.SET_LOADING, payload: false });
+            alert('account created redirecting...');
+            router.push('/login');
+        }
+
+
     }
 
     return (
@@ -45,24 +136,26 @@ export default function page() {
                     <form className='auth-form'>
                         <div className="form-input-wrapper">
                             <label htmlFor="name">Name</label>
-                            <input className='modal-input-item' type="text" id='name' onChange={(e) => setForm({ ...form, name: e.currentTarget.value })} />
+                            <input className='modal-input-item' type="text" id='name' onChange={(e) => dispatch({ type: REDUCER_ACTION_TYPE.SET_NAME, payload: e.target.value })} />
                         </div>
 
                         <div className="form-input-wrapper">
                             <label htmlFor="email">Email</label>
-                            <input className='modal-input-item' type="text" id='email' onChange={(e) => setForm({ ...form, email: e.currentTarget.value })} />
+                            <input className='modal-input-item' type="text" id='email' onChange={(e) => dispatch({ type: REDUCER_ACTION_TYPE.SET_EMAIL, payload: e.target.value })} />
+                            {state.email && !state.validEmail && <div className='error-message'>Invalid Email</div>}
                         </div>
 
                         <div className="form-input-wrapper">
                             <label htmlFor="pass">Create Password</label>
                             <div className="password-input-wrapper">
-                                <input className='modal-input-item' type="password" id='pass' onChange={(e) => setForm({ ...form, password: e.currentTarget.value })} />
+                                <input className='modal-input-item' type="password" id='pass' onChange={(e) => dispatch({ type: REDUCER_ACTION_TYPE.SET_PASSWORD, payload: e.target.value })} />
                                 <SvgIcon path='show-password' />
                             </div>
                             <p>Password must be at least 8 characters</p>
+                            {state.password && !state.validatePassword && <div className='error-message'>Invalid Password</div>}
                         </div>
 
-                        <button className="add-button" onClick={(handleClick)}>Create Account</button>
+                        <button type='button' className="add-button" onClick={handleClick} disabled={state.loading}>{state.loading ? 'Loading...' : 'Sign Up'}</button>
 
                         <div className="have-account">
                             <p>Already have an account ?</p>
